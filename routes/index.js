bb4903b116b81c8b2ae887ea342b25247753c6b0
robert wynn <wynncreations@gmail.com>
2018-08-31 10:01:45 -0500

    Css and view changes.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,24 @@
 const express           = require("express");
-const router            = express.Router();
+const router            = express.Router({mergeParams:true});
 const middleware        = require("../middleware");
+const passport          = require("passport");
+
+
 
 //Base route for site.
-router.get("/",middleware.isLoggedIn,(req,res)=>{
+router.get("/",middleware.isLoggedIn,(req,res,next)=>{
     res.render("landing");
 });
+
+router.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+router.post("/login",passport.authenticate("local",{
+    successRedirect: "/login",
+    failureRedirect: "/"
+},(req,res)=>{
+    
+}));
 
 module.exports = router;
