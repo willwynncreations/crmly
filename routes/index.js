91329befb19c675ec52e0422ca8e2ffe89b708ef
robert wynn <wynncreations@gmail.com>
2018-08-31 08:45:40 -0500

    Added logged in middleware.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const express           = require("express");
 const router            = express.Router();
+const middleware        = require("../middleware");
 
 //Base route for site.
-router.get("/",(req,res)=>{
+router.get("/",middleware.isLoggedIn,(req,res)=>{
     res.render("landing");
 });
-
-
 
 module.exports = router;
