3763aca10e42ded5994c721b2a49203badfe5cc7
robert wynn <wynncreations@gmail.com>
2018-09-04 17:28:24 -0500

    added mongoose-searchable package to make customer searches easier.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,15 @@ const express           = require("express");
 const router            = express.Router({mergeParams:true});
 const middleware        = require("../middleware");
 const passport          = require("passport");
+const Customer          = require("../models/customer");
+const Searchable        = requre("mongoose-searchable");
+
+
+router.post("/search",(req,res,next)=>{
+    //we need to search the DB for the term sent over by the search field and return a customer if found.
+    Customer.find();
+    //Return flash error if we don't find a customer.
+});
 
 //Base route for site.
 router.get("/",middleware.isLoggedIn,(req,res,next)=>{
